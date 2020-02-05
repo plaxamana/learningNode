4a8fcd1605723b02617e973b7c563533f05d789a
Philip Laxamana <philip.laxamana@hotmail.com>
2020-02-05 12:01:47 -0500

    initial files/ testing import in JS

---
+++ index.js
@@ -0,0 +1,10 @@
+import hugeNum from './myscript.js';
+
+const a = 2;
+const b = hugeNum;
+
+const product = function(a, b) {
+    return a * b;
+}
+
+console.log(product(a,b));
---
+++ myscript.js
@@ -0,0 +1,3 @@
+const hugeNum = 2020;
+
+export default hugeNum;
