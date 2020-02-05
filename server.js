bf09c38b1e9e191274ca5753514788de9847b0ad
Philip Laxamana <philip.laxamana@hotmail.com>
2020-02-05 16:21:55 -0500

    postman section

--- server.js
+++ server.js
@@ -2,7 +2,26 @@ import express from 'express';
 
 const app = express();
 
+app.use( (req, res, next ) => {
+    console.log('<h1>HELLO!!!</h1>');
+    next();
+});
+
 app.get('/', (req, res) => {
     res.send("Hello!!");
-})
-app.listen(3000);
+});
+
+app.get('/profile', (req, res) => {
+    res.send("getting profile");
+});
+
+app.post('/', (req, res) => {
+    const user = {
+        name: 'John',
+        hobby: 'snowboarding'
+    }
+    res.send(user);
+});
+
+
+app.listen(3000); 
